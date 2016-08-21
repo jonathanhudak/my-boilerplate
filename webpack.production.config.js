62a53df9afe1a321293ca736fc3b60ad4b2c3cb4
hudakdidit <hudak.jonathan@gmail.com>
2016-08-20 18:18:49 -0700

    Add css link for build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,10 +1,6 @@
-var webpack = require('webpack');
 var config = require('./webpack.config.js');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 var stylLoaderIndex = config.module.loaders.map(l => l.key).indexOf('styl');
-
-config.output.publicPath = '/public';
 
 config.module.loaders[stylLoaderIndex] = {
   test:   /\.styl$/,
